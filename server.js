e7d4d7f8bc913d3b4778f8e365429d381a5f935f
johndean3326 <113262365+johndean3326@users.noreply.github.com>
2023-04-21 20:50:35 -0600

    fixed connection issues

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/config');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(require('./controllers'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
